b361bffeffc68da84dfc9760e7fc55e0cb03d353
Lize Wu <retniw72@gmail.com>
2023-09-11 18:03:51 +0800

    更改 twitter icon

--- M/include_functiontop_public.php
+++ M/include_functiontop_public.php
@@ -1,9 +1,10 @@
 <div class="clearfix">
 	<div class="Function FunctionTop Public_Share hidden-xs pull-right">
 		<!-- Public -->
-		<a href="#" class="Twitter-BTN"><i class="icon-twitter"></i></a>
-		<a href="#" class="FB-BTN"><i class="icon-facebook-1"></i></a>
-		<a href="#" class="Plurk-BTN"><i class="icon-plurk"></i></a>
-		<a href="#" class="Google-BTN"><i class="icon-gplus"></i></a>
+		<a href="#" class="Twitter-BTN"><i><span>X (前身為 Twitter) </span></i></a>
+		<!-- <a href="#" class="Twitter-BTN"><i class="icon-twitter"><span>twitter</span></i></a> -->
+		<a href="#" class="FB-BTN"><i class="icon-facebook-1"><span>facebook</span></i></a>
+		<a href="#" class="Plurk-BTN"><i class="icon-plurk"><span>plurk</span></i></a>
+		<a href="#" class="Google-BTN"><i class="icon-gplus"><span>google plus</span></i></a>
 	</div>
 </div>
