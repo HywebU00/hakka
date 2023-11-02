94bb8b3bfca6d2de1a12c11efcb5b8f791413534
Lize Wu <retniw72@gmail.com>
2023-11-02 18:06:20 +0800

    L綠版，首頁slider 手機版換直圖、輪播調為5秒

--- L/include_slider.php
+++ L/include_slider.php
@@ -3,28 +3,29 @@
 	<div align="center"><img src="images/slider/001.jpg" alt="桐花" class="img-responsive"></div>
 </noscript>
 <section class="IndexSlide">
+	<!-- 1500*500 -->
 	<div class="pic">
 		<a href="" title="">
-			<img src="images/slider/001.png" alt="" class="img-web">
-			<img src="images/slider/001.png" alt="" class="img-rphone">
+			<img src="images/slider/001.jpg" alt="" class="img-web">
+			<img src="images/slider/001-m.jpg" alt="" class="img-phone">
 		</a>
 	</div>
 	<div class="pic">
 		<a href="" title="">
 			<img src="images/slider/002.jpg" alt="" class="img-web">
-			<img src="images/slider/002.jpg" alt="" class="img-rphone">
+			<img src="images/slider/001-m.jpg" alt="" class="img-phone">
 		</a>
 	</div>
 	<div class="pic">
 		<a href="" title="">
 			<img src="images/slider/003.jpg" alt="" class="img-web">
-			<img src="images/slider/003.jpg" alt="" class="img-rphone">
+			<img src="images/slider/001-m.jpg" alt="" class="img-phone">
 		</a>
 	</div>
 	<div class="pic">
 		<a href="" title="">
 			<img src="images/slider/004.jpg" alt="" class="img-web">
-			<img src="images/slider/004.jpg" alt="" class="img-rphone">
+			<img src="images/slider/001-m.jpg" alt="" class="img-phone">
 		</a>
 	</div>
 </section>
